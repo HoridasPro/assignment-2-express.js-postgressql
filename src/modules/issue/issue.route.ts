c1ff1011f705d1745add8b9a18659e4e59c60757
HoridasPro <horidassarker6@gmail.com>
2026-06-04 13:07:53 +0600

    updated to patch

--- src/modules/issue/issue.route.ts
+++ src/modules/issue/issue.route.ts
@@ -6,6 +6,6 @@ const router = Router();
 router.post("/", auth(), issuesController.createIssue);
 router.get("/", issuesController.getAllIssues);
 router.get("/:id", issuesController.getSingleIssue);
-router.put("/:id", auth(), issuesController.updateIssue);
+router.patch("/:id", auth(), issuesController.updateIssue);
 router.delete("/:id", auth(), issuesController.deleteIssueData);
 export const issuesRoute = router;
